41b5cd1cc645eee64571ce06e8495a374d382b57
paul rimiru <paulrimiru@gmail.com>
2017-11-27 10:52:55 +0300

    [Delivers #153127274] delivers first version of working endpoints

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,9 @@ Module contains all the routes required by the API
 from app import API as restful
 from app import views as funcs
 
-restful.add_resource(funcs.Register, '/api/auth/register')
-restful.add_resource(funcs.Authentication, '/api/auth/login')
-restful.add_resource(funcs.ResetPassword, '/api/auth/reset-password')
-restful.add_resource(funcs.CreateEvent, '/api/events')
-restful.add_resource(funcs.Event, '/api/events/<eventId>')
-restful.add_resource(funcs.Rsvp, '/api/event/<eventId>/rsvp')
+restful.add_resource(funcs.Register, '/api/v1/auth/register')
+restful.add_resource(funcs.Authentication, '/api/v1/auth/login')
+restful.add_resource(funcs.ResetPassword, '/api/v1/auth/reset-password')
+restful.add_resource(funcs.CreateEvent, '/api/v1/events')
+restful.add_resource(funcs.Event, '/api/v1/events/<eventId>')
+restful.add_resource(funcs.Rsvp, '/api/v1/event/<eventId>/rsvp')
