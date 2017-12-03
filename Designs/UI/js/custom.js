804587de673222945fa67217612425f7c986b5c8
paul rimiru <paulrimiru@gmail.com>
2017-12-03 15:18:18 +0300

    [Bug] Fix modals not appearing

--- Designs/UI/js/custom.js
+++ Designs/UI/js/custom.js
@@ -1,5 +1,15 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+    $('.modal').modal();
+    $(".datepicker").pickadate({
+      selectMonths: true, 
+      selectYears: 1,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: true
+    });
+    $('.tooltipped').tooltip({delay: 50});
     AOS.init({
         easing: 'ease-in-sine'
       });
