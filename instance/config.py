11b64c1a24467a049a1efd323b8af4abfa09fbf7
paul rimiru <paulrimiru@gmail.com>
2018-01-08 10:15:57 +0300

    [Chore] add more configurations for app configuration

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,26 @@
+# config.py
+
+import os
+
 class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = 'secret'
+    TESTING = False
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
     DEBUG = True
-    SECRET_KEY = "S3cr3T"
-    HOST='0.0.0.0'
+
+class TestingConfig(Config):
+    """Configurations for Testing"""
+    DEBUG = True
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
 app_config = {
-    'config':Config
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'configobj':Config,
+    'SECRET_KEY':'secret_key'
 }
